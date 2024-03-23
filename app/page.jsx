f73865a83a22ecb0e9d22afb870e1935c61b4b2e
rizk <rizkdotai@gmail.com>
2024-03-23 22:30:42 +0200

    setup project structure

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p4 max-w-3xl mx-auto">
+      <h1>Contact Us</h1>
+      <p>Please fill in the form below</p>
+    </div>
+  );
+}
+
